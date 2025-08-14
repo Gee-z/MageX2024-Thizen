caf265abdef17062c1fbc2fb2e1f708bb5cac4fc
GeezNuts <157205180+Gee-z@users.noreply.github.com>
2025-08-14 08:40:12 +0700

    almost all fixed other than missing 2 dialogue

--- Assets/Script/Hub/Crafting.cs
+++ Assets/Script/Hub/Crafting.cs
@@ -10,9 +10,9 @@ public class Crafting : MonoBehaviour
     public GameObject BluePrint;
     public void OpenCrafting()
     {
-        if(InventoryManager.instance != null)
+        if (InventoryManager.instance != null)
         {
-            if((InventoryManager.instance.CheckItem(1)&&InventoryManager.instance.CheckItem(2)&&InventoryManager.instance.CheckItem(3)))
+            if (InventoryManager.instance.CheckItem(1) && InventoryManager.instance.CheckItem(2) && InventoryManager.instance.CheckItem(3))
             {
                 BluePrint.SetActive(true);
             }
@@ -21,14 +21,14 @@ public class Crafting : MonoBehaviour
                 DialogueManager.instance.StartDialog(1);
             }
         }
-        else 
-                DialogueManager.instance.StartDialog(1);
+        else
+            DialogueManager.instance.StartDialog(1);
     }
     public void CheckCompletion()
     {
-        if(Vector2.Distance(Pieces[0].TargetPosition, Pieces[0].gameObject.transform.position) < Offset &&Vector2.Distance(Pieces[1].TargetPosition, Pieces[1].gameObject.transform.position)<Offset &&Vector2.Distance(Pieces[2].TargetPosition, Pieces[2].gameObject.transform.position)<Offset )
+        if (Vector2.Distance(Pieces[0].TargetPosition, Pieces[0].gameObject.transform.position) < Offset && Vector2.Distance(Pieces[1].TargetPosition, Pieces[1].gameObject.transform.position) < Offset && Vector2.Distance(Pieces[2].TargetPosition, Pieces[2].gameObject.transform.position) < Offset)
         {
-            if(InventoryManager.instance != null)
+            if (InventoryManager.instance != null)
             {
                 InventoryManager.instance.UseItem(1);
                 InventoryManager.instance.UseItem(2);
@@ -38,5 +38,5 @@ public class Crafting : MonoBehaviour
                 DialogueManager.instance.StartDialog(3);
             }
         }
-    }   
+    }
 }
